1c0077dad9b1dd73385c49e8eea6e485482eb15c
Fenhl <fenhl@fenhl.net>
2015-03-06 08:05:51 +0000

    Add Iron skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+extern crate iron;
+extern crate router;
+
+use iron::prelude::*;
+use iron::status;
+use router::Router;
+
+fn hello_world(_: &mut Request) -> IronResult<Response> {
+    Ok(Response::with((status::Ok, "Hello, world!")))
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut router = Router::new();
+    router.get("/", hello_world);
+    Iron::new(router).http("0.0.0.0:18800").unwrap();
 }
