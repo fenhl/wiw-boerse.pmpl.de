8162cdafa8d3c1333c0911dcfb79bc51a18270d7
Fenhl <fenhl@fenhl.net>
2015-03-08 13:39:51 +0000

    Add Bootstrap page structure and intro

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ extern crate iron;
 #[macro_use] extern crate lazy_static;
 extern crate router;
 extern crate "rustc-serialize" as rustc_serialize;
+//extern crate static;
 
 use std::error::Error;
 
@@ -37,6 +38,21 @@ impl std::fmt::Display for AuthError {
 impl Error for AuthError {
     fn description(&self) -> &str {
         "authentication error"
+    }
+}
+
+#[derive(Debug)]
+struct Nyi;
+
+impl std::fmt::Display for Nyi {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        std::fmt::Display::fmt("not yet implemented", f)
+    }
+}
+
+impl Error for Nyi {
+    fn description(&self) -> &str {
+        "not yet implemented"
     }
 }
 
@@ -74,14 +90,45 @@ fn check_auth(req: &mut Request) -> IronResult<()> {
     }
 }
 
-fn hello_world(_: &mut Request) -> IronResult<Response> {
-    Ok(Response::with((status::Ok, "text/html".parse::<Mime>().unwrap(), include_str!("../assets/index.html"))))
+fn index(_: &mut Request) -> IronResult<Response> {
+    Ok(Response::with((status::Ok, "text/html".parse::<Mime>().unwrap(), format!(r#"<!DOCTYPE html>
+<html>
+    <head>
+        {header}
+    </head>
+    <body>
+        {nav}
+        <div class="container" style="position: relative; top: 71px;">
+            <div class="panel panel-default">
+                {intro}
+            </div>
+        </div>
+    </body>
+</html>
+"#,
+        header=include_str!("../assets/header.html"),
+        intro=include_str!("../assets/intro.html"),
+        nav=include_str!("../assets/nav.html")
+    ))))
+}
+
+fn logo(_: &mut Request) -> IronResult<Response> {
+    Err(IronError {
+        error: Box::new(Nyi),
+        response: Response {
+            status: Some(status::NotImplemented),
+            headers: headers::Headers::new(),
+            extensions: TypeMap::new(),
+            body: None
+        }
+    })
 }
 
 fn main() {
     // route
     let mut router = Router::new();
-    router.get("/", hello_world);
+    router.get("/", index);
+    router.get("/logo.png", logo);
     // handle auth
     let mut chain = Chain::new(router);
     chain.link_before(check_auth);
